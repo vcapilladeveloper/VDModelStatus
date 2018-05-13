e1a6efe3cc39cafc3ab9a0b71c887867ee2d4cdc
Victor Capilla Borrego <victor.capilla@gmail.com>
2018-05-13 10:26:19 +0200

    Initial Commit

---
+++ VDModelStatus/VDModelStatus.h
@@ -0,0 +1,19 @@
+//
+//  VDModelStatus.h
+//  VDModelStatus
+//
+//  Created by Victor Capilla Borrego on 13/5/18.
+//  Copyright © 2018 Victor Capilla Borrego. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VDModelStatus.
+FOUNDATION_EXPORT double VDModelStatusVersionNumber;
+
+//! Project version string for VDModelStatus.
+FOUNDATION_EXPORT const unsigned char VDModelStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VDModelStatus/PublicHeader.h>
+
+
